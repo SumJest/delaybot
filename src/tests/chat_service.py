72ad8ce6e91a34924ed54d8969b6f2c9872fe43a
Roman <summedjesters@gmail.com>
2024-06-17 18:48:06 +0500

    update chat service test

--- src/tests/chat_service.py
+++ src/tests/chat_service.py
@@ -1,8 +1,13 @@
+import logging
 import unittest
+from unittest.mock import MagicMock
 
 from dependency_injector.wiring import inject, Provide
+from vkwave.bots import ForWhat
 
+from middlewares.types.states import AddGroupStates
 from models import User, Chat
+from resources import messages
 from services import ChatService
 from .container import ServicesContainer
 from .database.test import with_test_db
@@ -25,7 +30,34 @@ class ChatServiceTestCase(unittest.IsolatedAsyncioTestCase):
         user = User.create(
             user_id=123123
         )
-        event = MockObject()
-        await self.chat_service.create_chat(event, user, chat)
+        event = MagicMock()
+        await self.chat_service.create_chat(event=event,
+                                            user=user,
+                                            chat=chat)
+        self.chat_service.fsm.set_state.assert_called_once_with(event=event,
+                                                                state=AddGroupStates.NAMING,
+                                                                for_what=ForWhat.FOR_USER)
+        self.chat_service.api_context.messages.send.assert_called_once_with(
+            user_id=user.user_id,
+            message=messages.ADD_GROUP
+        )
         self.assertIsNotNone(chat.owner)
         self.assertEqual(chat.owner.user_id, user.user_id)
+
+    @with_test_db((User, Chat))
+    async def test_name_chat(self):
+        chat = Chat.create(
+            peer_id=123456
+        )
+        user = User.create(
+            user_id=123456
+        )
+        event = MagicMock()
+        event.object.object.message.text = "Test chat name"
+        await self.chat_service.name_chat(event=event,
+                                          user=user,
+                                          chat=chat)
+        chat = Chat.get(peer_id=123456)
+        self.chat_service.fsm.finish.assert_called_once_with(event=event,
+                                                             for_what=ForWhat.FOR_USER)
+        self.assertEqual(chat.name, "Test chat name")
