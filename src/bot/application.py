ea186ceaaa302c21f97bb34e7190b1eb79467df2
Roman <summedjesters@gmail.com>
2025-05-13 22:48:26 +0500

    setting commands

--- src/bot/application.py
+++ src/bot/application.py
@@ -1,4 +1,5 @@
 from aiogram import Bot, Dispatcher
+from aiogram.types import BotCommand, BotCommandScopeAllGroupChats, BotCommandScopeAllPrivateChats
 from dependency_injector.wiring import inject, Provide
 
 from bot.handlers import callback_router
@@ -21,3 +22,13 @@ async def setup(webhook_url,
         allowed_updates=dp.resolve_used_update_types(),
         drop_pending_updates=True
     )
+    group_commands = [
+        BotCommand(command="queue", description="Добавить очередь в беседу (укажите название)"),
+        BotCommand(command="queues", description="Список очередей беседы")
+    ]
+
+    private_commands = [
+    ]
+
+    await bot.set_my_commands(commands=group_commands, scope=BotCommandScopeAllGroupChats())
+    await bot.set_my_commands(commands=private_commands, scope=BotCommandScopeAllPrivateChats())
